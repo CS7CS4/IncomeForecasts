6ffeea43902aec989acdb305d80d5a97f7627eca
geniusAhua <648930239@qq.com>
2022-12-02 19:23:24 +0000

    improve the parameters

--- data_process.py
+++ data_process.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as py
 
 def main():
-    data = pd.read_csv('final_cleaned_data1.csv', encoding = 'utf-8')
+    data = pd.read_csv('final_cleaned_data2.csv', encoding = 'utf-8')
 
     # local_types = ('San Jose', 'New York', 'San Francisco', 'California')
     # data = pd.DataFrame(local_types, columns = ['location_filter'])
@@ -10,17 +10,18 @@ def main():
     #one-hot
     local_oh = pd.get_dummies(data.location_filter, prefix = 'location')
     job_oh = pd.get_dummies(data.job_title_filter, prefix = 'jobTitle')
+    exp_oh = pd.get_dummies(data.experience_filter, prefix = 'exp')
 
     data['Salary'] = data['Salary'].astype(float)
     data['Salary'] = data['Salary'].apply(lambda x: salary_(x))
 
-    experience_map = {
-            "Entry": 0,
-            "Mid": 1,
-            "Senior": 2
-        }
-    data['experience_filter'] = data['experience_filter'].map(experience_map)
-    output = pd.concat([job_oh, local_oh, data[['experience_filter', 'Salary']]], axis = 1)
+    # experience_map = {
+    #         "Entry": 0,
+    #         "Mid": 1,
+    #         "Senior": 2
+    #     }
+    # data['experience_filter'] = data['experience_filter'].map(experience_map)
+    output = pd.concat([job_oh, local_oh, exp_oh, data[['Salary']]], axis = 1)
 
     # data.drop(columns = ['Title'], inplace = True)
     # data.drop(columns = ['Company'], inplace = True)
@@ -40,7 +41,7 @@ def main():
     output.to_csv('dataset.csv', index = None, encoding = 'utf-8')
 
 def salary_(val):
-    if val >= 114000:
+    if val >= 117000:
         val = 1
     else:
         val = 0
--- knn_predict.py
+++ knn_predict.py
@@ -4,7 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import pandas as pd
 from sklearn import metrics
-from sklearn.metrics import classification_report, confusion_matrix, roc_curve
+from sklearn.metrics import auc, classification_report, confusion_matrix, roc_curve
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LogisticRegression
@@ -16,16 +16,22 @@ warnings.filterwarnings("ignore")
 #load dataset
 df = pd.read_csv('dataset.csv', sep=",", dtype=float)
 
-x11 = df.iloc[:, 0]
-x12 = df.iloc[:, 1]
-x13 = df.iloc[:, 2]
-x21 = df.iloc[:, 3]
-x22 = df.iloc[:, 4]
-x23 = df.iloc[:, 5]
-x24 = df.iloc[:, 6]
-x31 = df.iloc[:, 7]
-y = df.iloc[:, 8]
-X = np.column_stack((x11, x12, x13, x21, x22, x23, x24, x31))
+x_ = []
+for i in range(0, df.shape[1]):
+    if i != df.shape[1] - 1:
+        x_.append(df.iloc[:, i])
+    else:
+        y = df.iloc[:, i]
+# x11 = df.iloc[:, i]
+# x12 = df.iloc[:, 1]
+# x13 = df.iloc[:, 2]
+# x21 = df.iloc[:, 3]
+# x22 = df.iloc[:, 4]
+# x23 = df.iloc[:, 5]
+# x24 = df.iloc[:, 6]
+# x31 = df.iloc[:, 7]
+# y = df.iloc[:, 8]
+X = np.column_stack(tuple(x_))
 
 # poly = PolynomialFeatures(degree = 2, include_bias = False, interaction_only = False)
 # Xpoly = poly.fit_transform(X)
@@ -34,37 +40,37 @@ X = np.column_stack((x11, x12, x13, x21, x22, x23, x24, x31))
 # Xtrain, Xtest, ytrain, ytest = train_test_split(Xpoly_df, y, test_size=0.2, random_state=0)
 
 '''KNN>>>>'''
-#polynomial features
-k_fold_range = [5, 6, 7, 8, 9, 10]
-k_range = [3, 5, 7, 8, 9, 10, 11, 13, 15, 17, 19]
+# kfold = 5
+# k_range = [3, 5, 7, 8, 9, 10, 11, 13, 15, 17, 19]
 
+# mean_error = []; std_error = []
+# for k in k_range:
+#     model = KNeighborsClassifier(n_neighbors = k, weights = 'uniform')
+#     from sklearn.model_selection import cross_val_score
+#     scores = cross_val_score(model, X, y, cv = kfold, scoring = 'f1')
+#     mean_error.append(scores.mean())
+#     std_error.append(scores.std())
 
-for kfold in k_fold_range:
-    mean_error = []; std_error = []
-    for k in k_range:
-        model = KNeighborsClassifier(n_neighbors = k, weights = 'uniform')
-        from sklearn.model_selection import cross_val_score
-        scores = cross_val_score(model, X, y, cv = kfold, scoring = 'f1')
-        mean_error.append(scores.mean())
-        std_error.append(scores.std())
-
-    plt.errorbar(k_range, mean_error, yerr=std_error, linewidth=1, label = f'k-fold: {kfold}')
-plt.xlabel("K"); plt.ylabel("F1 Score")
-plt.legend()
-plt.show()
+# plt.errorbar(k_range, mean_error, yerr=std_error, linewidth=1, label = f'k-fold: {kfold}')
+# plt.xlabel("KNN-k"); plt.ylabel("F1 Score")
+# plt.legend()
+# plt.show()
 '''<<<<KNN'''
 
 #KNN
 model = KNeighborsClassifier(n_neighbors = 9, weights = 'uniform')
-Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2, random_state = 0)
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.6, random_state = 0)
 model.fit(Xtrain, ytrain)
 ypred_knn = model.predict(Xtest)
 print(">>>>>>>>>>>>>>>>>>>>>>KNN<<<<<<<<<<<<<<<<<<<<")
 print(confusion_matrix(ytest, ypred_knn))
 print(classification_report(ytest, ypred_knn))
 print("-------------------------------------------------------------")
+print(f'Accuracy of train: {model.score(Xtrain, ytrain)}')
+print(f'Accuracy of test: {model.score(Xtest, ytest)}')
 #draw roc curve
 fpr, tpr, _ = roc_curve(ytest,ypred_knn)
+print(f'AUC of KNN: {auc(fpr, tpr)}')
 plt.plot(fpr, tpr, label="knn", c="red")
 plt.xlabel("False positive rate")
 plt.ylabel("True positive rate")
